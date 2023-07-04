37d8f6dce326205e3c5feb61ad0bde29b0f4a265
Rani Hinnawi <rhinnawi95@gmail.com>
2023-07-04 16:31:17 -0700

    separated out formatter functions into new module

--- lab1/supporting_functions.py
+++ lab1/supporting_functions.py
@@ -10,33 +10,8 @@ Date: 2023-07-04
 from typing import TextIO
 from lab1.prefix_to_postfix import PrefixToPostfix
 from lab1.performance import Performance
-
-
-def generate_output(line_number: int, prefix: str, result: str, metrics: str,
-                    error=False) -> str:
-    """
-    Function that formats the inputted expression and the output given from the
-    prefix to postfix conversion.
-
-    Args:
-        line_number: integer for labelling lines in the output
-        expression: prefix expression string
-        result: postfix expression string OR error message
-        error: boolean indicating whether result is an error message
-
-    Returns:
-        string conditionally formatted based off result type
-    """
-    write = f"{line_number}. Prefix: {prefix}"
-
-    if (error):
-        write += f"\n\tERROR - {result}\n"
-    else:
-        write += f" || Postfix: {result}\n"
-
-    write += f"{metrics}\n"
-
-    return write + '\n'
+from lab1.output_formatters import format_conversion_results, \
+    format_performance_report
 
 
 def run_conversions(input_file: TextIO, output_file: TextIO) -> None:
@@ -56,6 +31,8 @@ def run_conversions(input_file: TextIO, output_file: TextIO) -> None:
 
     with open(input_file, 'r', encoding="utf-8") as file, \
             open(output_file, 'w', encoding="utf-8") as out:
+        out.write("-------Conversion Results-------\n")
+
         # Counting lines for clean formatting
         line_counter = 1
 
@@ -81,26 +58,10 @@ def run_conversions(input_file: TextIO, output_file: TextIO) -> None:
                 else:
                     performance.log_success()
 
-                out.write(generate_output(line_counter, prefix_expression,
-                                          result, str(performance), error=error))
+                out.write(format_conversion_results(line_counter, prefix_expression,
+                                                    result, str(performance), error=error))
                 line_counter += 1
 
-        out.write("\n")
-        out.write(
-            f"Total number of successes: {performance.get_num_successes()}\n")
+        out.write(format_performance_report(performance))
 
-        successes = performance.get_successes()
-        for size in sorted(successes.keys()):
-            runtime = successes[size]
-            out.write(f"{size}: {runtime}\n")
-
-        out.write(f"Total number of errors: {performance.get_num_errors()}\n")
-
-        out.write("\n")
-
-        errors = performance.get_errors()
-        for size in sorted(errors.keys()):
-            runtime = errors[size]
-            out.write(f"{size}: {runtime}\n")
-
-        out.write("Done")
+        out.write("\nDone")
